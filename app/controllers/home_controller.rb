c650ff60600b98c7703db1e8e14fc37a0a4b27e1
agilesonic <agilesonic@gmail.com>
2012-09-13 10:44:24 -0400

    ""

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,18 @@ class HomeController < ApplicationController
   end
   
   def upload
+    file = params["file"]
+    @results = Hash.new
+    file.each do |line|
+      v = line.split(',')
+      type = v[2]
+      val = to_i(v[6])
+      if @results[type].ni?
+        @result[type] = val
+      else
+        @result[type] += val
+      end
+    end
     render "result"
   end
 
