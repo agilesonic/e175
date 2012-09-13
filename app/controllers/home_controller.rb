942a36cb4b8c21483f9e52f0b477855c1d9b0db1
agilesonic <agilesonic@gmail.com>
2012-09-13 11:01:27 -0400

    ""

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
   
   def upload
-    file = params["file"]
+    file = params["file"].tempfile
     @results = Hash.new
     file.each do |line|
       v = line.split(',')
